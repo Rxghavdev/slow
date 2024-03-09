84945a6c0bdd828178b21e797eab7eeb79e165f5
Rxghavdev <mattaraghav67@gmail.com>
2024-03-09 20:21:42 +0530

    added loader and fixed vid

--- server.js
+++ server.js
@@ -153,8 +153,9 @@ app.get('/speedtest', async (req, res) => {
     }
 });
 
+
 // Start the server
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
